20689a4af2f6a0d220dae18cc4a7f007a93dccb1
Juho Teperi <juho.teperi@iki.fi>
2013-10-11 20:58:27 +0300

    Improved documentation

--- README.md
+++ README.md
@@ -1,3 +1,106 @@
 # Random functions
 
-Source code is the documentation
+Some functions Lodash is missing but I have found useful.
+
+Documentation contains some usage examples.
+In addition to simple examples there are some particulary
+good use cases using AngularJS + Socket.io.
+
+
+## Array manipulation
+
+Angularjs data-binding breaks if you try to replace array in
+$scope with a copy of array e.g. a new Object.
+So if you want to append or prepend data to a collection in the scope you
+should modify the current Array.
+
+### _.prepend(array, element)
+
+```
+var a = [1, 2];
+_.prepend(a, -1); // a = [-1, 1, 2];
+_.prepend(a, [3, 4]); // a = [3, 4, -1, 1, 2];
+
+socket.on('users:new', _.partial(_.prepend, $scope.users));
+```
+
+### _.append(array, element)
+
+```
+var a = [1, 2];
+_.append(a, -1); // a = [1, 2, -1];
+_.append(a, [3, 4]); // a = [1, 2, -1, 3, 4];
+
+socket.on('users:list', _.partial(_.append, $scope.users));
+```
+
+### _.removeFirst(array, callback)
+
+Remove the first matching object from collection.
+If the callback is [POJ](http://lodash.com/docs#isPlainObject) element
+[_.findIndex](http://lodash.com/docs#findIndex) is used to find element to be removed.
+Otherwise [_.indexOf](http://lodash.com/docs#indexOf) is used.
+
+```
+var a = ['a', 'b', 'b', {k: 1, foo: 2}, {k: 2, foo: 3}, {k: 1, foo: 2}];
+_.removeFirst(a, 'b'); // a = ['a', 'b', {k: 1, foo: 2}, {k: 2, foo: 3}, {k: 1, foo: 2}];
+_.removeFirst(a, {k: 1}); // a = ['a', 'b', {k: 2, foo: 3}, {k: 1, foo: 2}];
+
+// user:removed events first parameter is e.g. {_id: 'foo'}
+socket.on('user:removed', _.partial(_.removeFirst, $scope.users));
+```
+
+### _.removeAll(array, callback)
+
+Like _.removeFirst except removes all matches instead of the first.
+
+
+## Object manipulation
+
+### _.set(object, key, value)
+
+Set property in target object to given value
+
+```
+var a = {a: 1, b: 2};
+_.set(a, 'a', 3); // a = {a: 3, b: 2};
+```
+
+```
+socket.on('state', function (data) {
+  $scope.state = data;
+});
+// or
+socket.on('state', _.partial(_.set, $scope, 'state'));
+```
+
+### _.get(object, key)
+Alias for [_.result](http://lodash.com/docs#result).
+
+## Misc
+
+### _.construct(Constructor, value)
+
+If value is defined use given constructor to create a new Object else return undefined.
+
+```
+new Date(undefined) // = Invalid Date
+// or
+_.construct(Date, undefined) // = undefined
+```
+
+### _.clean
+
+Returns a deep copy of the object with empty or undefined properties removed.
+
+```
+var q = {user: user._id};
+if (request.params.from) q.from = {$lt: new Date(request.params.from)};
+// or
+var q = _.clean({
+  user: user._id,
+  from: {$lt: _.construct(Date, request.params.from)} // request.params.from = undefined
+}); // q = {user: 'foo'}
+```
+
+Though in this case the first in fact looks better...
--- src/random-lodash-mixins.js
+++ src/random-lodash-mixins.js
@@ -8,15 +8,6 @@
 
   var mixins = {};
 
-  /*
-  If value is defined use given constructor to create a new Object.
-
-  new Date(undefined) => Invalid Date
-
-  vs.
-
-  _.construct(Date, undefined) => undefined
-  */
   mixins.construct = function construct(Target, value) {
     if (typeof value !== 'undefined') {
       return new Target(value);
@@ -25,21 +16,6 @@
     }
   };
 
-  /*
-  Returns a deep copy of the object with empty or undefined properties removed.
-
-  Use case: Mongo query
-
-  var q = {user: user._id};
-  if (request.params.from) q.from = {$lt: new Date(request.params.from)};
-
-  vs.
-
-  var q = _.clean({
-    user: user._id,
-    from: {$lt: _.construct(Date, request.params.from)}
-  });
-  */
   mixins.clean = function clean(object) {
     var r = {};
 
@@ -61,11 +37,6 @@
     return r;
   };
 
-  /*
-  Prepends data to a array.
-
-  socket.on('users:new', _.partial(_.prepend, $scope.users));
-  */
   mixins.prepend = function prepend(collection, data) {
     if (_.isArray(data)) {
       _.partial(Array.prototype.splice, 0, 0).apply(collection, data);
@@ -75,11 +46,6 @@
     return collection;
   };
 
-  /*
-  Append data to a array.
-
-  socket.on('users:list', _.partial(_.append, $scope.users));
-  */
   mixins.append = function append(collection, data) {
     if (_.isArray(data)) {
       Array.prototype.push.apply(collection, data);
@@ -89,9 +55,6 @@
     return collection;
   };
 
-  /*
-  Internal helper
-  */
   function indexOf(callback) {
     if (_.isPlainObject(callback)) {
       return _.findIndex;
@@ -100,12 +63,6 @@
     }
   }
 
-  /*
-  Remove the first matching object from collection.
-
-  // Event data eg. {_id: '...'}
-  socket.on('user:removed', _.partial(_.removeFirst, $scope.users));
-  */
   mixins.removeFirst = function removeFirst(collection, callback) {
     var i = indexOf(callback)(collection, callback);
 
@@ -115,9 +72,6 @@
     return collection;
   };
 
-  /*
-  Remove all matching objects from collection.
-  */
   mixins.remove = function remove(collection, callback) {
     var i;
     var find = indexOf(callback);
@@ -128,67 +82,16 @@
     return collection;
   };
 
-  /*
-  Set property in target object to given value
-
   socket.on('state', function (data) {
     $scope.state = data;
   });
 
-  vs.
-
-  socket.on('state', _.partial(_.set, $scope, 'state'));
-  */
   mixins.set = function set(target, key, value) {
     target[key] = value;
     return target;
   };
 
-  /*
-  Get a property from object if it exists.
-  */
   mixins.get = _.result;
-
-  /*
-  Pattern matching
-
-  var a = undefined; a = {}; a = 5; a = "running"; a = [{a: 'collection'}]
-  _.match(a, [
-    ["running", function () console.log('...')],
-    [_.isUndefined, function () console.log('Input was undefined')],
-    [_.isEmpty, function () console.log('Input was empty')],
-    [Number, function () console.log('Input was a number')],
-    [_.and(_.isArray, _.partialRight(_.all, _.partial(_.has, 'a'))), function () console.log('Collections every element has property \'a\'')],
-    function () console.log('Default')
-  ]);
-  */
-  mixins.match = function match(input, patterns) {
-    patterns = _.rest(arguments);
-
-    _.some(patterns, function (pattern) {
-      // Default case
-      if (_.isFunction(pattern)) {
-        pattern(input);
-        return true;
-      }
-
-      // pattern.length should always be 2
-      var test = pattern[0];
-      var cb = pattern[1];
-      if (
-        _.isFunction(test) && (
-          (input !== undefined && input !== null && input.constructor === test) || // (5).constructor === Number
-          test(input) // _.isEmpty({})
-        ) ||
-        input.valueOf() === test.valueOf() // "running".valueOf() === "running".valueOf()
-      ) {
-        cb(input);
-        return true; // stop forEach
-      }
-    });
-
-    return input;
-  };
 
   _.mixin(mixins);
 }).call(this);
